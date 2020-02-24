f67a86bad95756e0dab873fb8b425275c03dbd61
LittleCadet <sx255879@163.com>
2020-02-24 22:17:02 +0800

    1.实现手动刷新 2.实现config server和client接入eureka

--- src/main/java/com/cloud/config/CloudConfigClientApplication.java
+++ src/main/java/com/cloud/config/CloudConfigClientApplication.java
@@ -2,13 +2,13 @@ package com.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author LittleCadet
  * @Date 2020/2/23
  */
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudConfigClientApplication
 {
